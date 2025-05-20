94de2cfc7b2da1446aaf83b6496a08299fcad6c9
wangzheng <wangzheng000001@gmail.com>
2025-05-20 21:34:33 +0900

    建援隊javaバックエンド

---
+++ kenentai/src/main/java/com/seisoul/kenentai/KenentaibackendApplication.java
@@ -0,0 +1,13 @@
+package com.seisoul.kenentai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KenentaibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KenentaibackendApplication.class, args);
+	}
+
+}
---
+++ kenentai/src/test/java/com/seisoul/kenentai/KenentaibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seisoul.kenentai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KenentaibackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
